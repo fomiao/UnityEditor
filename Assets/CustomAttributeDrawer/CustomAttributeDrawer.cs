484398c75b4a3f5563dc7eebed4d16dfb05be706
fomiao <66628251@qq.com>
2019-09-12 16:39:59 +0800

    CustomAttributeDrawer

---
+++ Assets/CustomAttributeDrawer/CustomAttributeDrawer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//定义中文attribute
+public class LabelAttribute : PropertyAttribute
+{
+    public string label;
+    public LabelAttribute(string label)
+    {
+        this.label = label;
+    }
+}
+
+
+
+
+
---
+++ Assets/CustomAttributeDrawer/Editor/CustomPropertyDrawEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomPropertyDrawer(typeof(LabelAttribute), false)]
+public class LabelAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        label.text = (attribute as LabelAttribute).label;
+        EditorGUI.PropertyField(position, property, label);
+    }
+}
